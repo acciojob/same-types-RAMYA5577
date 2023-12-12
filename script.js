30ecb5bca06808f586066313eab678cbcfce03db
RAMYA5577 <121097897+RAMYA5577@users.noreply.github.com>
2023-12-12 09:10:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,13 +1,8 @@
 function isSameType(value1, value2) {
-	var ans=false;
-  if (isNaN(value1) && isNaN(value2)) {
- ans=true;
-  }
-
-  else if(typeof value1 === typeof value2){
-	  ans=true;
-  }
-	return ans;
+if(isNaN(value1) && isNaN(value2)){
+	return true;
+}
+	return typeof value1===typeof value2;
 }
 
 
